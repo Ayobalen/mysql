0f778b2728ce34ec4f1d79d7e555558ef788884e
Balen <idrisrasheed2000@gmail.com>
2022-10-15 00:52:05 +0100

    update

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const mysqlConnection = mysql.createConnection({
   })
 
   app.listen(3000, ()=> console.log("Server is running on port 3000"))
-
+// get all employee
   app.get('/employees',(req, res)=>{
     mysqlConnection.query
     ('SELECT * FROM Employee',(err, rows, fields)=>{
@@ -30,11 +30,35 @@ const mysqlConnection = mysql.createConnection({
         console.log(err)
    } )
   })
+  // Get an employee
   app.get('/employees/:id',(req, res)=>{
     mysqlConnection.query
     ('SELECT * FROM Employee WHERE EmpID = ?',[req.params.id],(err, rows, fields)=>{
         if(!err)
-      res.send(rows )
+      res.send(rows)
+        else
+        console.log(err)
+   } )
+  })
+ //Delete an employee
+  app.delete('/employees/:id',(req, res)=>{
+    mysqlConnection.query
+    ('DELETE FROM Employee WHERE EmpID = ?',[req.params.id],(err, rows, fields)=>{
+        if(!err)
+      res.send("Deleted successfully")
+        else
+        console.log(err)
+   } )
+  })
+
+  //Insert an employee
+  app.post('/employees',(req, res)=>{
+    let emp = req.body
+    var sql = "SET @EmpID = ?, SET @Name = ?; SET @EmpCode = ?, SET @Salary = ?; \
+    CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);"
+    mysqlConnection.query(sql,[emp.EmpID, emp.Name, emp.EmpCode, emp.Salary],(err, rows, fields)=>{
+        if(!err)
+      res.send(rows)
         else
         console.log(err)
    } )
